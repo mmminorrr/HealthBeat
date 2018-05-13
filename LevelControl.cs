4554efb2a9874b93bffb776597d3ba53c5ced79f
mmminorrr <minor.mk@hotmail.com>
2018-05-13 22:23:19 +0700

    Add files via upload

---
+++ LevelControl.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour {
+
+
+    public Sprite easy;
+    public Sprite normal;
+    public Sprite hard;
+
+    public bool easyState;
+    public bool normalState = false;
+    public bool hardState = false;
+
+
+    private bool _inputLocked;
+    public float inputlockingTime = 1f;
+
+
+    // Use this for initialization
+    void Start () {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = easy;
+        easyState = true;
+    }
+
+
+    void UnlockInput()
+    {
+        _inputLocked = false;
+    }
+    void LockInput()
+    {
+        _inputLocked = true;
+        Invoke("UnlockInput", inputlockingTime);
+    }
+    public bool isInputLocked()
+    {
+        return _inputLocked;
+    }
+
+    void Update()
+    {
+        //normalState = false;
+        //hardState = false;
+
+        if (Input.GetMouseButtonDown(1) && easyState == true)
+        {
+            if (isInputLocked())
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = normal;
+                Debug.LogWarning("Input locked --- Normal");
+                normalState = true;
+                easyState = false;
+                hardState = false;
+            }
+
+            else
+            {
+                Debug.Log("Hello!");
+                LockInput();
+            }
+        }
+
+        else if(Input.GetMouseButtonDown(1) && normalState == true)
+        {
+            if (isInputLocked())
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = hard;
+                Debug.LogWarning("Input locked --- Hard");
+                hardState = true;
+                easyState = false;
+                normalState = false;
+            }
+
+            else
+            {
+                Debug.Log("Hello!");
+                LockInput();
+            }
+        }
+
+        else if (Input.GetMouseButtonDown(1) && hardState == true)
+        {
+            if (isInputLocked())
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = easy;
+                Debug.LogWarning("Input locked --- Easy");
+                easyState = true;
+                normalState = false;
+                hardState = false;
+            }
+
+            else
+            {
+                Debug.Log("Hello!");
+                LockInput();
+            }
+        }
+    }
+
+}
---
+++ Performance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Performance : MonoBehaviour {
+
+    public Sprite begin_p;
+    public Sprite perfect;
+    public Sprite good;
+    public Sprite wrong;
+
+    public GestureSourceManager getPerform;
+	// Use this for initialization
+	void Start () {
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = begin_p;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(getPerform.scoreValue >= 20)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = perfect;
+        }
+        else if(getPerform.scoreValue >= 10 && getPerform.scoreValue < 20)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = good;
+        }
+        else if(getPerform.scoreValue < 10)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = wrong;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = begin_p;
+        }
+
+	}
+}
